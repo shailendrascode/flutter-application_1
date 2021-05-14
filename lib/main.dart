e7e001b941948787a04c53885aea800c14a1400b
shailendrascode <mail2shailendranow@gmail.com>
2021-05-14 16:42:05 +0530

    learning basic structure with classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp(MaterialApp(
-    title: 'Sample app',
-    theme: ThemeData(
-      primarySwatch: Colors.red,
-      ),
-      home: Center(
-        child: Text(
-          'Hi from me'
-        )
-      )
+void main(List<String> args) {
+  runApp(MyApp());
+}
 
-  )
-  );
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Sample app",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "App"
+          ),
+
+          
+          
+        ),
+
+        body: Center(
+          child: Text("i am shailendra"),
+        ),
+      ),
+
+      
+    );
+  }
 }
