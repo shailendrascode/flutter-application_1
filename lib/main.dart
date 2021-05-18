26165ce09347d30a70f632ea61a7f4d0fd1367ab
shailendrascode <mail2shailendranow@gmail.com>
2021-05-18 21:37:26 +0530

    restoring the app to previos state after wrong merging in git

--- lib/main.dart
+++ lib/main.dart
@@ -12,45 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Sample app",
+      themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "App"
-          ),
-
-          
-          
-        ),
-
-        body: Center(
-          child: TextButton(
-            
-            onPressed: () {},
-            style: new ButtonStyle(
-              visualDensity: new VisualDensity(horizontal: 1, vertical: 2),
-              backgroundColor: MaterialStateProperty.all(Colors.green)
-            ),
-             child: Text(
-               "Flat button",
-               style: new TextStyle(
-                 color: Colors.green,
-                 background: new Paint(
-
-                 )
-                 
-               ),
-               
-               )
-             )
-        ),
-      ),
-
-      
+          primarySwatch: Colors.lightBlue,
+          fontFamily: GoogleFonts.lato().fontFamily),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        MyRoutes.homeroute: (context) => Home(),
+        MyRoutes.loginroute: (context) => LoginPage(),
+      },
     );
   }
 }
